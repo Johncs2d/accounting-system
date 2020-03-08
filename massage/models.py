2145dba2285441c43f923ce17551b2594dc3106e
Johncs2d <34950240+Johncs2d@users.noreply.github.com>
2020-03-08 15:31:17 +0800

    models finalized

--- massage/models.py
+++ massage/models.py
@@ -27,7 +27,7 @@ class serviceInfo(models.Model):
 	service_category =  models.ForeignKey(service_category,on_delete=models.PROTECT,null=True,related_name="service_category")
 	service_description = models.CharField(max_length=100)
 	service_price = models.DecimalField(max_digits = 6, decimal_places = 2)
-	service_income_account = models.ForeignKey(chartofaccounts,on_delete=models.PROTECT,null=True,related_name="income_account")
+	service_income_account = models.ForeignKey(chartofaccounts,on_delete=models.PROTECT,null=False,related_name="income_account")
 	def __str__(self):
 		return f"Service Name: {self.service_name} | Service Sku: {self.service_sku} | Service Category: {self.service_category} | Service Description: {self.service_description} | Service Price: {self.service_price} | Service Income Account: {self.service_income_account}"
 
@@ -41,9 +41,41 @@ class companyInfo(models.Model):
 	company_state = models.CharField(max_length=100)
 	company_zip = models.CharField(max_length=100)
 
-	def __init__(self):
+	def __str__(self):
 		return f"{self.company_name} | {self.company_phone} | {self.company_fax} | {self.company_country} | {self.company_city} | {self.company_state} | {self.company_zip}"
 
-class journal(object):
-	account_name = models.ForeignKey(chartofaccounts,on_delete=models.PROTECT,null=True,related_name="journal_account")
-	pass
+class journalmain(models.Model):
+	datecreate = models.DateField(("Date"), default=date.today)
+	def __str__(self):
+		return f"Journal ID {self.pk} | Date Created {self.datecreated}"
+		
+class journalcollections(models.Model):
+	transaction_date = models.DateField(("Date"), null=False)
+	account_id = models.ForeignKey(chartofaccounts,on_delete=models.PROTECT,null=True,related_name="journal_account")
+	debits = models.DecimalField(max_digits = 6, decimal_places = 2)
+	credits = models.DecimalField(max_digits = 6, decimal_places = 2)
+	description = models.CharField(max_length=200)
+	journalid = models.ForeignKey(journalmain,on_delete=models.PROTECT,null=False,related_name="journals")
+	def __str__(self):
+		return f"{self.transaction_date} | {self.account_id}  | $ {self.debits} | $ {self.credits} | {self.description} | Journal #{self.journalid.id}"
+
+class employees(models.Model):
+	employee_name = models.CharField(max_length=100)
+	employee_city = models.CharField(max_length=100)
+	employee_status = models.CharField(max_length=20)
+	employee_position = models.CharField(max_length=20)
+	def __str__(self):
+		return f"{self.employee_name} | {self.employee_city} | {self.employee_status} | {self.employee_position}"
+
+class logs(models.Model):
+	event_name = models.CharField(max_length=100)
+	account_involved = models.ForeignKey(chartofaccounts,on_delete=models.PROTECT,null=True,related_name="logs_account")
+	amount = models.DecimalField(max_digits = 6, decimal_places = 2)
+	newbalance = models.DecimalField(max_digits = 6, decimal_places = 2)
+	date = models.DateField(("Date"), default=date.today)
+	def __str__(self):
+		return f"{self.event_name} | {self.account_involved} | {self.amount} | {self.newbalance} | {self.date}"
+
+	
+		
+		
