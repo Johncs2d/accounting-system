ce8bc0b4d23b277b714bdadb8b222d431df35163
Johncs2d <34950240+Johncs2d@users.noreply.github.com>
2020-03-11 14:03:42 +0800

    update functions

--- massage/urls.py
+++ massage/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('info', views.info, name='info'),
-    path('chart', views.chartofaccounts, name='chart'),
+    path('chart', views.charts, name='chart'),
     path('trialbalance', views.trialbalance, name='trialbalance'),
     path('ledger', views.ledger, name='ledger'),
     path('balancesheet', views.balancesheet, name='balancesheet'),
     path('incomestatement', views.incomestatement, name='incomestatement'),
     path('receive', views.receive, name='receive'),
     path('journalize', views.journalize, name='journalize'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path("insertaccount", views.insertaccount, name='insertaccount'),
+    path("inserjournal", views.inserjournal, name='inserjournal'),
 ]
